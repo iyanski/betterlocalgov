dee07b91e7727e498f03bb8d9d484d2442e0cb89
iyanski <me@iantusil.com>
2025-09-24 17:48:03 +0100

    Use react-helmet instead of react-helmet-async

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NuqsAdapter } from 'nuqs/adapters/react';
-import { HelmetProvider } from 'react-helmet-async';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import ScrollToTop from './components/ui/ScrollToTop';
 import Services from './pages/Services';
 import Document from './pages/Document';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <HelmetProvider>
-      <Router>
-        <NuqsAdapter>
-          <div className="min-h-screen flex flex-col">
-            <Navbar />
-            <ScrollToTop />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/services/:category" element={<Services />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/:lang/:documentSlug" element={<Document />} />
-              <Route path="/:documentSlug" element={<Document />} />
-            </Routes>
-            <Footer />
-          </div>
-        </NuqsAdapter>
-      </Router>
-    </HelmetProvider>
+    <Router>
+      <NuqsAdapter>
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services/:category" element={<Services />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/:lang/:documentSlug" element={<Document />} />
+            <Route path="/:documentSlug" element={<Document />} />
+          </Routes>
+          <Footer />
+        </div>
+      </NuqsAdapter>
+    </Router>
   );
 }
 
